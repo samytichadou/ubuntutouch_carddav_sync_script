78258c3521ebcf00950cfa9c459c8074a00d228d
samytichadou <samytichadou@gmail.com>
2024-05-18 18:09:59 +0200

    Add files via upload

--- carddav_config.sh
+++ carddav_config.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# Auto setup of carddav contacts
+
+# samytichadou@gmail.com
+#
+# Created by Samy Tichadou (tonton)
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Based on a script by Sebastian Gallehr <sebastian@gallehr.de>
+# Thanks to: Sebastian Gallehr <sebastian@gallehr.de>
+# Thanks to: Tiago Carrondo <tcarrondo@ubuntu.com>
+# Thanks to: Romain Fluttaz <romain@botux.fr>
+# Thanks to: Wayne Ward <info@wayneward.co.uk>
+# Thanks to: Mitchell Reese <mitchell@curiouslegends.com.au>
 
 ### Menu
 while [[ True ]];
